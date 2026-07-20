0b475b38707a0faa116974c6fa1160ce8e729389
Christian Poppe Terán <christian@poppe-teran.net>
2026-07-20 17:04:21 +0200

    Update decadal_statistics.sh

--- data/processing/decadal_statistics.sh
+++ data/processing/decadal_statistics.sh
@@ -3,7 +3,6 @@ ifile=/p/scratch/cjibg31/jibg3105/data/HOLIDROUGHT/SXI_DETECT/SXI_92D.nc
 odir=/p/scratch/cjibg31/jibg3105/data/HOLIDROUGHT/SXI_DETECT/decadal/
 var=SXI_P
 oprefix=${var}_92D_
-osuffix=_dfreq
 dec1=1960
 dec2=2010
 thresh=-1
@@ -16,10 +15,10 @@ for y0 in $(seq $dec1 10 $dec2); do
   echo "Processing ${y0}-${y1}"
   cdo -selyear,${y0}/${y1} -selvar,$var $ifile $tmp
   echo "  - computing statistics: relative drought time"
-  cdo -timmean -lec,$thresh $tmp  ${odir}/${oprefix}${y0}${osuffix}.nc
+  cdo -timmean -lec,$thresh $tmp  ${odir}/${oprefix}${y0}_dfreq.nc
   echo "  - computing statistics: min drought index"
-  cdo -timmin $tmp  ${odir}/${oprefix}${y0}${osuffix}.nc
+  cdo -timmin $tmp  ${odir}/${oprefix}${y0}_min.nc
   echo "  - computing statistics: longest drought spell"
-  cdo -mulc,$tstep -timmax -consecsum -lec,$thresh $tmp ${odir}/${oprefix}${y0}${osuffix}.nc
+  cdo -mulc,$tstep -timmax -consecsum -lec,$thresh $tmp ${odir}/${oprefix}${y0}_maxspell.nc
   rm -f "$tmp"
 done
